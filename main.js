9e38779946fb39b9a9b2f26aaad5f863f7957b29
Emmanouil Raptakis <emmanraptak@gmail.com>
2026-08-07 22:36:55 +0800

    pass 2nd test

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-console.log("Hello, JavaScript!")
+const lines = require('fs').readFileSync(0, 'utf-8').trim().split('\n');
+const a = Number(lines[0]);
+const b = Number(lines[1]);
+console.log(a+b)
