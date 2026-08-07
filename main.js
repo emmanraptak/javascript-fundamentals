eb0da7fe03763d8ec007c841ca889bbb365f2cad
Emmanouil Raptakis <emmanraptak@gmail.com>
2026-08-07 22:40:53 +0800

    pass 3rd test

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const lines = require('fs').readFileSync(0, 'utf-8').trim().split('\n');
 const a = Number(lines[0]);
 const b = Number(lines[1]);
-console.log(a+b)
+const c = Number(lines[2]);
+const mean = Math.floor((a + b + c) / 3);
+console.log(mean);
