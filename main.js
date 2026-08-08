8bba43058e1ccf0f9ba4c15b6463bef0c872dbee
Emmanouil Raptakis <emmanraptak@gmail.com>
2026-08-08 13:30:50 +0800

    pass 8th test

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
+function square(n){
+    return n * n;
+}
+
 const lines = require('fs').readFileSync(0, 'utf-8').trim().split('\n');
 const number = Number(lines[0]);
-var count = 0;
-for (let i = 1; i <= number; i++) {
-    count += i;
-}
-console.log(count);
+console.log(square(number));
