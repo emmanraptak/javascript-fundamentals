3138eba2ac43d004907854743a51b6b1630b33d2
Emmanouil Raptakis <emmanraptak@gmail.com>
2026-08-08 13:02:10 +0800

    pass 7th test

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
 const lines = require('fs').readFileSync(0, 'utf-8').trim().split('\n');
 const number = Number(lines[0]);
-
-if (number % 3 === 0 && number % 5 === 0) {
-    console.log("FizzBuzz");
-} else if (number % 3 === 0) {
-    console.log("Fizz");
-} else if (number % 5 === 0 ) {
-    console.log("Buzz");
-} else {
-    console.log(number);
+var count = 0;
+for (let i = 1; i <= number; i++) {
+    count += i;
 }
+console.log(count);
