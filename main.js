f6dae9416cff29dd4059731ecb9beb99fc2e311a
Emmanouil Raptakis <emmanraptak@gmail.com>
2026-08-08 14:50:13 +0800

    pass 11th test

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
-const nums = require('fs').readFileSync(0, 'utf-8').trim().split(' ').map(Number);
-var max = Math.max(...nums);
-console.log(max);
+const words = require('fs').readFileSync(0, 'utf-8').trim().split(' ');
+const seen = {};
+for (word of words) {
+    seen[word] = true;
+}
+console.log(Object.keys(seen).length);
