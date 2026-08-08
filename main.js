4886e7fb390d4def76ed04dab1e6ffe45d14c9c9
Emmanouil Raptakis <emmanraptak@gmail.com>
2026-08-08 12:52:43 +0800

    pass 5th test

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
-const word = require('fs').readFileSync(0, 'utf-8').trim();
-const reversed_word = word.split('').reverse().join('');
-console.log(reversed_word);
+const lines = require('fs').readFileSync(0, 'utf-8').trim().split('\n');
+const name = lines[0];
+const age = lines[1];
+console.log(`Hi, ${name}! You are ${age} years old.`);
