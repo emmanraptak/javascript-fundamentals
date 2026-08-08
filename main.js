17760fc7ab9cbef516febadaf4dcfd00e8491a56
Emmanouil Raptakis <emmanraptak@gmail.com>
2026-08-08 12:58:05 +0800

    pass 6th test

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 const lines = require('fs').readFileSync(0, 'utf-8').trim().split('\n');
-const name = lines[0];
-const age = lines[1];
-console.log(`Hi, ${name}! You are ${age} years old.`);
+const number = Number(lines[0]);
+
+if (number % 3 === 0 && number % 5 === 0) {
+    console.log("FizzBuzz");
+} else if (number % 3 === 0) {
+    console.log("Fizz");
+} else if (number % 5 === 0 ) {
+    console.log("Buzz");
+} else {
+    console.log(number);
+}
