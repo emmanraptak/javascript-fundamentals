4899ccfb25ed16dcbb46507d9172de3547b23ddb
Emmanouil Raptakis <emmanraptak@gmail.com>
2026-08-08 14:20:00 +0800

    pass 10th test

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-const max = (a, b) => a >= b ? a : b;
-
-const lines = require('fs').readFileSync(0, 'utf-8').trim().split('\n');
-const number1 = Number(lines[0]);
-const number2 = Number(lines[1]);
-console.log(max(number1, number2));
+const nums = require('fs').readFileSync(0, 'utf-8').trim().split(' ').map(Number);
+var max = Math.max(...nums);
+console.log(max);
